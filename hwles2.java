e379fb982222e3650873a6684becedb3977cb40b
huyhuy2607 <122993900+huyhuy2607@users.noreply.github.com>
2023-01-31 06:56:22 +0900

    Add files via upload

---
+++ hwles2.java
@@ -0,0 +1,63 @@
+
+
+public class hwles2 {
+    public static void main(String[] args) {
+        //bai 1 
+        //for (int i = 0; i <= 100; i++) {
+        //     System.out.println(i);
+        // }
+        
+        
+        // baj 3 
+        //for (int i = 0; i <= 100; i++) {
+        //     if(i%3 == 0){
+        //         System.out.println(i);
+        //     }
+
+        // bai 5 
+        //int num, sum = 0;
+        // try (Scanner sc = new Scanner(System.in)) {
+        //     System.out.println("Nhập vào một số: ");
+        //     num = sc.nextInt();
+        // }
+        // for(int i = 1; i <= num; ++i)
+        // {
+            
+        //     sum = sum + i;
+        // }
+        // System.out.println("Tổng các số tự nhiên từ 1 đến "+ num +" là: "+sum);
+        
+        // bai 7
+        // int num, sum = 0;
+        // try (Scanner sc = new Scanner(System.in)) {
+        //     System.out.println("Nhập vào một số: ");
+        //     num = sc.nextInt();
+        // }
+        // for(int i = 2; i <= num; ++i)
+        // {
+        //     if(i%2==0);
+        //     sum = sum + i;
+        // }
+        // System.out.println("Tổng các số tự nhiên từ 1 đến "+ num +" là: "+sum);
+
+
+        //bai8
+        // int num, sum = 0;
+        // try (Scanner sc = new Scanner(System.in)) {
+        //     System.out.println("Nhập vào một số: ");
+        //     num = sc.nextInt();
+        // }
+        // for(int i = 2; i <= num; ++i)
+        // {
+        //     if(i%5==0);
+        //     sum = sum + i;
+        // }
+        // System.out.println("Tổng các số tự nhiên từ 1 đến "+ num +" là: "+sum);
+
+
+        }
+     }
+       
+            
+        
+
